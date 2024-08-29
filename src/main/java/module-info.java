a38a0a7d85bb70beecdb0f43cd31aa19cfa30442
Victor Gabriel <victorgabriel4123@gmail.com>
2024-08-29 16:33:57 -0300

    Nova estrutura do projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.p3.jogodamemoria {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.p3.jogodamemoria to javafx.fxml;
+    exports com.p3.jogodamemoria;
+}
