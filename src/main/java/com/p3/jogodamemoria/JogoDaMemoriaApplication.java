fbb02599e4a29f3ad75641f4bc954efe6721b69f
gabrielDellrado <grddourado@cosmobots.io>
2024-09-14 16:49:12 -0300

    feat: tela para adicionar jogadores

--- src/main/java/com/p3/jogodamemoria/JogoDaMemoriaApplication.java
+++ src/main/java/com/p3/jogodamemoria/JogoDaMemoriaApplication.java
@@ -4,14 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class JogoDaMemoriaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JogoDaMemoriaApplication.class.getResource("jogoDaMemoriaView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 892, 700);
+        Parent root = FXMLLoader.load(getClass().getResource("JogoView.fxml"));
+        Scene scene = new Scene(root, 892, 700);
         stage.setTitle("Jogo da Memória");
         stage.setScene(scene);
         stage.show();
